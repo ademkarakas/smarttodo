1c7f1383a56f6ea29e9db6c7d120a9bb19812d1b
Adem Karakas <karakasadem38@gmail.com>
2025-05-13 20:15:31 +0200

    Änderung zur Sicherheit bei settings.py

--- smarttodo/settings.py
+++ smarttodo/settings.py
@@ -150,10 +150,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'antonmeierentwickler@gmail.com'
-EMAIL_HOST_PASSWORD = 'hdcksfczrfpfdnek'
-DEFAULT_FROM_EMAIL = 'antonmeierentwickler@gmail.com'
-SERVER_EMAIL = 'antonmeierentwickler@gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
 
 PASSWORD_RESET_TIMEOUT = 259200
 
