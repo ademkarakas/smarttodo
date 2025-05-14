25e0664f6ec062eed0646ae6be20cd1567774415
Adem Karakas <karakasadem38@gmail.com>
2025-05-14 15:05:05 +0200

    Änderung bei settings.py für Datenbank

--- smarttodo/settings.py
+++ smarttodo/settings.py
@@ -43,6 +43,13 @@ if 'DATABASE_URL' in os.environ:
             conn_health_checks=True,
         )
     }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Application definition
@@ -88,18 +95,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'smarttodo.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
